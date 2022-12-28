2eb5bd5259e2cc51afcb2dfc2b110904f3c25965
Jimmy Wu <wwf417888479@gmail.com>
2022-12-28 22:37:51 +1100

    fix: On demand fetch

--- personal-website/pages/api/revalidateBlogs.ts
+++ personal-website/pages/api/revalidateBlogs.ts
@@ -4,10 +4,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.query.secret !== process.env.NEXT_SECRET_TOKEN) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
-
   try {
     await res.revalidate("/blogs");
     return res.json({ revalidated: true });
