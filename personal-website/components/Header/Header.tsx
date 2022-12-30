438722196a649bc0e99209a54e9fb44e8253a92b
Jimmy Wu <wwf417888479@gmail.com>
2022-12-30 18:40:29 +1100

    perf: Header does't show any buttons before confirm user login status

--- personal-website/components/Header/Header.tsx
+++ personal-website/components/Header/Header.tsx
@@ -82,6 +82,7 @@ const Header = observer(() => {
   const [dropdownVisible, setDropdownVisible] = useState(false);
   const { userStore } = useContext(RootStoreContext);
   const { asPath } = useRouter();
+  const [alreadyCheckUserLogin, setAlreadyCheckUserLogin] = useState(false);
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
@@ -104,6 +105,7 @@ const Header = observer(() => {
           );
         });
       }
+      setAlreadyCheckUserLogin(true);
     });
   }, []);
 
@@ -114,111 +116,120 @@ const Header = observer(() => {
   return (
     <nav className='bg-white absolute z-50 w-full border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-800 opacity-90 hover:opacity-80'>
       <div className='container flex flex-wrap items-center justify-between mx-auto'>
-        <a className='flex items-center' href='/'>
+        <a
+          className={`flex items-center ${
+            alreadyCheckUserLogin ? "" : "md:pt-3 md:pb-3 pt-1 pb-1"
+          }`}
+          href='/'
+        >
           <Image src={WSlogon} className='w-6 h-6 mr-3 ' alt='W slogon' />
           <span className='self-center text-xl font-semibold whitespace-nowrap dark:text-white'>
             Jimmy's Personal
           </span>
         </a>
-        <button
-          data-collapse-toggle='navbar-default'
-          type='button'
-          className='inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
-          aria-controls='navbar-default'
-          aria-expanded='false'
-        >
-          <span className='sr-only'>Open main menu</span>
-          <svg
-            className='w-6 h-6'
-            aria-hidden='true'
-            fill='currentColor'
-            viewBox='0 0 20 20'
-            xmlns='http://www.w3.org/2000/svg'
-            onClick={() => toggleDropdown()}
-          >
-            <path
-              fillRule='evenodd'
-              d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
-              clipRule='evenodd'
-            ></path>
-          </svg>
-        </button>
-        <div
-          className={`${
-            dropdownVisible ? "" : "hidden"
-          }  w-full md:block md:w-auto`}
-          id='navbar-default'
-        >
-          <ul className='flex flex-col items-center justify-center p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 '>
-            {HEADER_BUTTONS_CONFIG.map((header_button_config) => (
-              <li key={header_button_config.text}>
-                <HeaderInButton
-                  text={header_button_config.text}
-                  link={header_button_config.link}
-                  selected={asPath === header_button_config.link}
-                />
-              </li>
-            ))}
-            {userStore.userID ? (
-              <>
-                {SIGNINED_HEADER_BUTTONS_CONFIG.map((config) => (
-                  <li
-                    key={config.text}
-                    className={`flex flex-row items-center justify-center gap-x-2 rounded-lg ${config.style}`}
-                  >
-                    <span className='text-white'>{config.icon}</span>
+        {alreadyCheckUserLogin && (
+          <>
+            <button
+              data-collapse-toggle='navbar-default'
+              type='button'
+              className='inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
+              aria-controls='navbar-default'
+              aria-expanded='false'
+            >
+              <span className='sr-only'>Open main menu</span>
+              <svg
+                className='w-6 h-6'
+                aria-hidden='true'
+                fill='currentColor'
+                viewBox='0 0 20 20'
+                xmlns='http://www.w3.org/2000/svg'
+                onClick={() => toggleDropdown()}
+              >
+                <path
+                  fillRule='evenodd'
+                  d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
+                  clipRule='evenodd'
+                ></path>
+              </svg>
+            </button>
+            <div
+              className={`${
+                dropdownVisible ? "" : "hidden"
+              }  w-full md:block md:w-auto`}
+              id='navbar-default'
+            >
+              <ul className='flex flex-col items-center justify-center p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 '>
+                {HEADER_BUTTONS_CONFIG.map((header_button_config) => (
+                  <li key={header_button_config.text}>
                     <HeaderInButton
-                      text={config.text}
-                      link={config.link}
-                      selected={asPath === config.link}
+                      text={header_button_config.text}
+                      link={header_button_config.link}
+                      selected={asPath === header_button_config.link}
                     />
                   </li>
                 ))}
-                <li>
-                  <button
-                    className={
-                      "block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                    }
-                    onClick={(e) => {
-                      e.preventDefault();
-                      fetch("/api/sign-out")
-                        .then((response) => {
-                          if (response.status === 200) {
-                            userStore.userLogout();
-                          } else {
-                            console.log("Fail to log out users");
-                          }
-                        })
-                        .catch((e) => {
-                          console.log("Error when sign out users");
-                          console.log(e);
-                        });
-                    }}
-                  >
-                    {"Sign Out"}
-                  </button>
-                </li>
-              </>
-            ) : (
-              <>
-                <li>
-                  <HeaderInButton
-                    text={"Login"}
-                    link={"/login"}
-                    selected={false}
-                  />
-                </li>
-                <li>
-                  <HeaderInButton
-                    text={"Signup"}
-                    link={"/signup"}
-                    selected={false}
-                  />
-                </li>
-              </>
-            )}
-          </ul>
-        </div>
+                {userStore.userID ? (
+                  <>
+                    {SIGNINED_HEADER_BUTTONS_CONFIG.map((config) => (
+                      <li
+                        key={config.text}
+                        className={`flex flex-row items-center justify-center gap-x-2 rounded-lg ${config.style}`}
+                      >
+                        <span className='text-white'>{config.icon}</span>
+                        <HeaderInButton
+                          text={config.text}
+                          link={config.link}
+                          selected={asPath === config.link}
+                        />
+                      </li>
+                    ))}
+                    <li>
+                      <button
+                        className={
+                          "block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                        }
+                        onClick={(e) => {
+                          e.preventDefault();
+                          fetch("/api/sign-out")
+                            .then((response) => {
+                              if (response.status === 200) {
+                                userStore.userLogout();
+                              } else {
+                                console.log("Fail to log out users");
+                              }
+                            })
+                            .catch((e) => {
+                              console.log("Error when sign out users");
+                              console.log(e);
+                            });
+                        }}
+                      >
+                        {"Sign Out"}
+                      </button>
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li>
+                      <HeaderInButton
+                        text={"Login"}
+                        link={"/login"}
+                        selected={false}
+                      />
+                    </li>
+                    <li>
+                      <HeaderInButton
+                        text={"Signup"}
+                        link={"/signup"}
+                        selected={false}
+                      />
+                    </li>
+                  </>
+                )}
+              </ul>
+            </div>
+          </>
+        )}
       </div>
     </nav>
   );
