918f0f9316aef4fa2deab906c7ce9e8f76f7695e
Jimmy Wu <wwf417888479@gmail.com>
2022-12-31 15:41:08 +1100

    perf: editor and input styles

--- personal-website/components/Input/Input.tsx
+++ personal-website/components/Input/Input.tsx
@@ -27,7 +27,7 @@ const Input = ({
       isError
         ? "focus:border-red-500 border-red-500 dark:border-red-500"
         : "border-gray-300 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:ring-blue-500  dark:focus:ring-blue-500 dark:focus:border-blue-500"
-    } bg-gray-50 border  text-gray-900 text-sm rounded-lg  block w-full p-2.5 dark:bg-gray-700 `}
+    } bg-gray-50 border opacity-80 text-gray-900 text-sm rounded-lg  block w-full p-2.5 dark:bg-gray-700 `}
     placeholder={placeHolder}
     required={required}
     value={value}
